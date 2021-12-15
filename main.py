45326c9e764ca98492907ad07df3a66b455c0cbc
Hummird <vladbak2000@gmail.com>
2021-12-16 01:04:41 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import pygame
 import math
 
-#lol
-def draw_rect(angle):
+def coordinates(angle):
+    
     height=50
     width=100
 
     #calculate the starting pos
-    new_angle = 90- angle
+    near_angle = 90 - angle
 
-    hypo = math.sqrt(pow(math.tan(new_angle*0.01745329) * height,2) + pow(height,2))
-    side1 = math.cos(angle*0.01745329) * width
-    side2 = math.sin(angle*0.01745329) * width
-    side3 = math.sin(angle*0.01745329) * height
-    side4 = math.sqrt(pow(height,2) - pow(side3,2))
+    xtl = math.sqrt(pow(math.tan(near_angle*0.01745329) * height,2) + pow(height,2)) #initial X pos of TOP LEFT point 
+    tlx_to_trx = math.cos(angle*0.01745329) * width #length from initial X pos of TOP LEFT point to initial X pos of TOP RIGHT point
+    ytr = math.sin(angle*0.01745329) * width #initial Y pos of TOP RIGHT point
+    xtl_to_xbl = math.sin(angle*0.01745329) * height #length from initial X pos of TOP LEFT point to initial X pos of BOTTOM LEFT point
+    ybl = math.sqrt(pow(height,2) - pow(xtl_to_xbl,2)) #initial Y pos of BOTTOM LEFT point
 
-    #a = [ax+c,ay+c]
-    #...
-    i=side2*2
-    j=hypo*2
-    a = [hypo+j,0+i]
-    b = [side1+hypo+j,side2+i]
-    c = [hypo-side3+j,side4+i]
-    d = [side1+hypo-side3+j,side2+side4+i]
+    #[ax,ay,bx,by,cx,cy,dx,dy]
+    return [xtl, 0, xtl_to_xtr+xtl, ytr, xtl-xtl_to_xbl, ybl, xtl_to_xtr+xtl-xtl_to_xbl, ytr+ybl]
+
+
+def draw_rect(coords,distance):
+
+    #displacement_x=
+    #displacement_y=
+
+    a = [coords[0],coords[1]]
+    b = [coords[2],coords[3]]
+    c = [coords[4],coords[5]]
+    d = [coords[6],coords[7]]
 
     #draw it out
     pygame.draw.line(screen, RED, a, b, 5) 
@@ -30,7 +35,7 @@ def draw_rect(angle):
     pygame.draw.line(screen, RED, b, d, 5) 
     pygame.draw.line(screen, RED, c, d, 5) 
 
-def force():
+def starting_input():
     print("define angle")
     angle = int(input())
     print("define mass")
@@ -44,31 +49,44 @@ def force():
     print(force)
     return angle,force
 
-angle,force = force()
-#opening a window
-pygame.init()
-width = 720
-height = math.tan(angle*0.01745329) * width
-print(height)
-size=(width,height)
-screen= pygame.display.set_mode(size)
-pygame.display.set_caption("ajhla")
+def main():
+    #input all of the variables
+    angle,force = starting_input()
 
-# Define some colors
-BLACK = ( 0, 0, 0)
-WHITE = ( 255, 255, 255)
-GREEN = ( 0, 255, 0)
-RED = ( 255, 0, 0)
+    #calculate the initial coordinates of a rectangle
+    coords = coordinates(angle)
 
-carryOn = True
-clock = pygame.time.Clock()
- 
-while carryOn:
-    screen.fill(WHITE)
-    pygame.draw.line(screen, GREEN, [0, 0], [width, height], 5)
-    draw_rect(angle)
-    pygame.display.flip()
+    #opening a window
+    pygame.init()
+    width = 720
+    height = math.tan(angle*0.01745329) * width
 
-    clock.tick(60)
-     
-pygame.quit()
+    print("opening window ",width," x ",height)
+    screen= pygame.display.set_mode((width,height))
+    pygame.display.set_caption("friction force")
+
+    # Define some colors
+    BLACK = ( 0, 0, 0)
+    WHITE = ( 255, 255, 255)
+    GREEN = ( 0, 255, 0)
+    RED = ( 255, 0, 0)
+    BLUE = (0, 0, 255)
+
+    #starting the main render loop
+    carryOn = True
+    clock = pygame.time.Clock()
+    while carryOn:
+
+        for event in pygame.event.get():
+            #close button closes the window
+            if event.type == pygame.QUIT:
+                carryOn = False
+        
+        screen.fill(WHITE)
+        pygame.draw.line(screen, GREEN, [0, 0], [width, height], 5)
+        draw_rect(angle)
+
+        pygame.display.flip()
+        clock.tick(60)
+         
+    pygame.quit()
