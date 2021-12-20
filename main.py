6d9f12139c963272191255f43fe50c37460ce371
Hummird <vladbak2000@gmail.com>
2021-12-21 02:13:22 +0300

    added fixed resolution and centering of a track

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import tkinter #for getting screen resolution
 import pygame #for rendering an output
 import math
 
-def coordinates(angle):
+#converts any angle to rad
+def rad(angle):
+    return angle * 0.01745329 
+
+def coordinates(angle,move_x,move_y):
     
+    #block dimentions 
     height=50
     width=100
 
-    near_angle = 1.570796 - angle
+    near_angle = rad(90) - angle #adjacent angle in rad
+
     #calculate the starting pos
     xtl = math.sqrt(pow(math.tan(near_angle) * height,2) + pow(height,2)) #initial X pos of TOP LEFT point 
     xtl_to_xtr = math.cos(angle) * width #length from initial X pos of TOP LEFT point to initial X pos of TOP RIGHT point
     ytr = math.sin(angle) * width #initial Y pos of TOP RIGHT point
     xtl_to_xbl = math.sin(angle) * height #length from initial X pos of TOP LEFT point to initial X pos of BOTTOM LEFT point
     ybl = math.sqrt(pow(height,2) - pow(xtl_to_xbl,2)) #initial Y pos of BOTTOM LEFT point
+    
+    #if we move down tne we move the block back to a good position
+    if (move_y >= 50):
+        move_y -= 50
+        move_x = math.tan(near_angle) * ybl * -1
 
     #[ax,ay,bx,by,cx,cy,dx,dy]
-    return [xtl, 0, xtl_to_xtr+xtl, ytr, xtl-xtl_to_xbl, ybl, xtl_to_xtr+xtl-xtl_to_xbl, ytr+ybl]
+    output = [xtl+move_x, 0+move_y, xtl_to_xtr+xtl+move_x, ytr+move_y, xtl-xtl_to_xbl+move_x, ybl+move_y, xtl_to_xtr+xtl-xtl_to_xbl+move_x, ytr+ybl+move_y]
+    print(output)
+    return output
 
 def draw_rect(angle,coords,distance):
 
@@ -42,13 +55,17 @@ def starting_input():
     speed0 = float(input("define initial velocity\n"))
 
     #convert degrees to radian
-    angle = angle * 0.01745329 
+    angle = rad(angle)
 
-    #calculate friction force
-    force = mu*mass*9.8*math.cos(angle)
+    if (angle > rad(90)):
+        force = 0
+        acceleration = 9.8
+    else:
+        #calculate friction force
+        force = mu*mass*9.8*math.cos(angle)
 
-    #calculate the acceleration
-    acceleration=9.8*(math.sin(angle)-mu*math.cos(angle))
+        #calculate the acceleration
+        acceleration=9.8*(math.sin(angle)-mu*math.cos(angle))
     
     return speed0,angle,force,acceleration
 
@@ -56,24 +73,47 @@ def main():
     #input all of the variables
     speed0,angle,force,acceleration = starting_input()
 
-    #calculate the initial coordinates of a rectangle
-    coords = coordinates(angle)
-
     #getting screen resolution
     root = tkinter.Tk()
-    width = root.winfo_screenwidth()
-    height = root.winfo_screenheight()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
     root.destroy()
+
+    #calculating width and height of a window
+    #and width and heigth of a track
+    if (screen_width >= 1080): screen_width = 1080
+
+    height = math.tan(angle) * screen_width
+    if (height < 600):
+        screen_height = 600
+    elif (height > screen_height-100):
+        screen_height -= 100
+        height = screen_height
+    else:
+        screen_height = height
+
+    #w/o it the line won't draw out for some reason
+    if (angle==rad(0)):
+        width=screen_width
+    else:
+        width = math.tan(1.570796 - angle) * height
+
+
+    #calculating needed x and y displacement for a center aligned track
+    move_x=(screen_width - abs(width))/2
+    #prevents it from moving up in case of extreme angles
+    move_y=0
+    if(height < screen_height):
+        move_y=(screen_height - height)/2
+
+    #calculate the initial coordinates of a rectangle
+    coords = coordinates(angle,move_x,move_y)
 
     #opening a window
     pygame.init()
-    
-    if (width >= 1080): width = 1080
-    height = math.tan(angle) * width
-
-    print("opening window ",width," x ",height)
+    print("opening window ",screen_width," x ",screen_height)
     global screen
-    screen = pygame.display.set_mode((width,height))
+    screen = pygame.display.set_mode((screen_width,screen_height))
     pygame.display.set_caption("friction force")
     font = pygame.font.Font('Ubuntu.ttf', 30)
 
@@ -96,9 +136,13 @@ def main():
             #close button closes the window
             if event.type == pygame.QUIT:
                 window_open = False
-
         
         while render:
+            #duplicate the same loop here or  else the close button breaks
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    window_open = False
+                    render = False
             time+=1/60
             speed=speed0 + acceleration*time
             if (speed<0): speed=0
@@ -106,17 +150,17 @@ def main():
             distance = speed0*time + (acceleration*math.pow(time,2))/2
 
             screen.fill(WHITE)
-            pygame.draw.line(screen, GREEN, [0, 0], [width, height], 5)
+            pygame.draw.line(screen, GREEN, [0+move_x, 0+move_y], [width+move_x, height+move_y], 5)
             draw_rect(angle,coords,distance)
             
             string_time="time:"+str(round(time,6))
             string_speed="speed:"+str(round(speed,6))
             string_force="force:"+str(round(force,6))
             string_acceleration="acceleration:"+str(round(acceleration,6))
-            screen.blit(font.render(string_time, True, BLUE),(width-200,0))
-            screen.blit(font.render(string_speed, True, BLUE),(width-200,25))
-            screen.blit(font.render(string_force, True, RED),(10,height-65))
-            screen.blit(font.render(string_acceleration, True, RED),(10,height-40))
+            screen.blit(font.render(string_time, True, BLUE),(screen_width-200,0))
+            screen.blit(font.render(string_speed, True, BLUE),(screen_width-200,25))
+            screen.blit(font.render(string_force, True, RED),(10,screen_height-65))
+            screen.blit(font.render(string_acceleration, True, RED),(10,screen_height-40))
 
             pygame.display.flip()
 
