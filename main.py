c8a40f88814436dbb4615adf2d89d4e1c5573387
Hummird <vladbak2000@gmail.com>
2021-12-22 01:06:52 +0300

    версия на русском

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-import tkinter #for getting screen resolution
+import tkinter #для нахождения разрешения экрана
 import math
-import pygame #for rendering an output
-#for randomizing stuff in the beginning
+import pygame #для рендера модели
+#для рандомизации входных данных
 from random import randrange 
 from random import random
 
-#converts any angle to rad
+#переводит градусы в радианы
 def rad(angle):
     return angle * 0.01745329 
 
 def coordinates(angle,move_x,move_y):
     
-    #block dimentions 
+    #размеры блока 
     height=50
     width=100
 
-    near_angle = rad(90) - angle #adjacent angle in rad
+    near_angle = rad(90) - angle #смежный угол в радианах
 
-    #calculate the starting pos
-    xtl = math.sqrt(pow(math.tan(near_angle) * height,2) + pow(height,2)) #initial X pos of TOP LEFT point 
-    xtl_to_xtr = math.cos(angle) * width #length from initial X pos of TOP LEFT point to initial X pos of TOP RIGHT point
-    ytr = math.sin(angle) * width #initial Y pos of TOP RIGHT point
-    xtl_to_xbl = math.sin(angle) * height #length from initial X pos of TOP LEFT point to initial X pos of BOTTOM LEFT point
-    ybl = math.sqrt(pow(height,2) - pow(xtl_to_xbl,2)) #initial Y pos of BOTTOM LEFT point
+    #вычисление стартовой позиции
+    xtl = math.sqrt(pow(math.tan(near_angle) * height,2) + pow(height,2)) #стартовая X позиция верхней левой точки (XTopLeft) 
+    xtl_to_xtr = math.cos(angle) * width #расстояние от стартовой X позиции верхней левой точки до стартовой X позиции верхней правой точки (XTopLeft_to_XTopRight)
+    ytr = math.sin(angle) * width #стартовая Y позиция верхней правой точки (YTopRight)
+    xtl_to_xbl = math.sin(angle) * height #расстояние от стартовой X позиции верхней левой точки до стартовой X позиции нижней левой точки (XTopLeft_to_XBottomLeft)
+    ybl = math.sqrt(pow(height,2) - pow(xtl_to_xbl,2)) #стартовая Y позиция нижней левой точки (YBottomLeft)
     
-    #if we move down tne we move the block back to a good position
+    #если мы двигаем модель вниз, то мы двигаем блок влево на хорошую позицию
     if (move_y >= 50):
         move_y -= 50
         move_x = math.tan(near_angle) * ybl * -1
@@ -39,11 +39,12 @@ def draw_track(angle,distance,move_x,move_y):
     x_b = x_a - math.sin(angle)*20
     y_b = y_a * 2
 
-    #if we move down tne we move the block back to a good position
+    #если мы двигаем модель вниз, то мы двигаем штрихи влево на более удачную позицию
     if (move_y >= 20):
         move_y -= 20
         move_x = math.tan(rad(90)-angle) * y_a * -1
 
+    #мы рисуем 100 штрихов с запасом на любой размер окна
     for i in range(-10,90):
         d_x = math.cos(angle)*(i*20 + distance)
         d_y = math.sin(angle)*(i*20 + distance)
@@ -51,7 +52,7 @@ def draw_track(angle,distance,move_x,move_y):
     
 def draw_rect(angle,coords,distance):
 
-    #calculate the displacement
+    #вычисление сдвига
     displacement_x = math.cos(angle) * distance
     displacement_y = math.sin(angle) * distance
 
@@ -60,7 +61,7 @@ def draw_rect(angle,coords,distance):
     c = [coords[4]+displacement_x,coords[5]+displacement_y]
     d = [coords[6]+displacement_x,coords[7]+displacement_y]
 
-    #draw it out
+    #отрисовка
     pygame.draw.line(screen, RED, a, b, 5) 
     pygame.draw.line(screen, RED, a, c, 5) 
     pygame.draw.line(screen, RED, b, d, 5) 
@@ -69,94 +70,95 @@ def draw_rect(angle,coords,distance):
     return displacement_x,displacement_y
 
 def starting_input():
-    randomize=bool(int(input("do you want me to randomize this? enter 1 or 0\n")))
+    randomize=bool(int(input("рандомизировать входные данные? введите 1 или 0\n")))
     if (randomize==True):
         angle=randrange(0,90)
         mass=randrange(0,9000)
         mu=random()
         speed0=randrange(0,100)
     else:
-        angle=float(input("define angle\n"))
+        angle=float(input("введите угол\n"))
         while (angle<0) or (angle>90):
-            angle = float(input("please enter an angle between 0 and 90\n"))
-        mass = float(input("define mass\n"))
+            angle = float(input("пожалуйста, введите угол между 0 и 90 градусами\n"))
+        mass = float(input("введите массу\n"))
         while (mass<0):
-            mass = float(input("sadly, it's not a helium baloon, so please enter a mass of at least 0\n"))
-        mu = float(input("define how slippery\n"))
+            mass = float(input("к сожалению, это не гелиевый шарик, пожалуйста введите массу хотя бы 0\n"))
+        mu = float(input("введите коэффициент скольжения\n"))
         while (mu<0) or (mu>1):
-            mu = float(input("this cannot be less than 0 and more than 1, try again\n"))
-        speed0 = float(input("define initial velocity\n"))
+            mu = float(input("эта величина не может быть меньше 0 или больше 1, попробуйте снова\n"))
+        speed0 = float(input("введите начальную скорость\n"))
         while (speed0<0):
-            speed0 = float(input("please enter a speed that's at least 0\n"))
+            speed0 = float(input("пожалуйста, введите скорость хотя бы 0\n"))
 
-    print("your starting parameters are")
-    print("angle =",angle)
-    print("mass =",mass)
-    print("mu =",mu)
-    print("initial speed =",speed0)
-    #convert degrees to radian
+    print("ваши входные параметры:")
+    print("угол =",angle)
+    print("масса =",mass)
+    print("коэф. скольжения =",mu)
+    print("начальная скорость =",speed0)
+
+    #переводим угол в радианы
     angle = rad(angle)
 
-    #tidying up a tiny measurment error
+    #подправляем погрешность в вычислениях
     if(angle==rad(90)):
         force=0
         acceleration=9.8
     else:
-        #calculate friction force
+        #вычисляем силу трения
         force = mu*mass*9.8*math.cos(angle)
 
-        #calculate the acceleration
+        #вычисляем ускорение
         acceleration=9.8*(math.sin(angle)-mu*math.cos(angle))
     
     return speed0,angle,force,acceleration
 
 def main():
-    #input all of the variables
+    #вводим все переменные
     speed0,angle,force,acceleration = starting_input()
 
-    #getting screen resolution
+    #получае разрешение экрана
     root = tkinter.Tk()
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     root.destroy()
 
-    #calculating width and height of a window
-    #and width and heigth of a track
+    #вычисляем высоту и ширину окна
+    #и высоту и ширину плоскости
     if (screen_width >= 1080): screen_width = 1080
 
     height = math.tan(angle) * screen_width
-    if (height < 600):
-        screen_height = 600
+    if (height < 400):
+        screen_height = 400
     elif (height > screen_height-100):
         screen_height -= 100
         height = screen_height
     else:
         screen_height = height
 
-    #w/o it the line won't draw out for some reason
+    #без этого он почему-то не отрисовывает плоскость
     if (angle==rad(0)):
         width=screen_width
     else:
         width = math.tan(rad(90) - angle) * height
 
-    #calculating needed x and y displacement for a center aligned track
+    #вычисляем нужный сдвиг чтоб модель была в центре
     move_x=(screen_width - abs(width))/2
-    #prevents it from moving up in case of extreme angles
+    #предотвращение сдвига вверх на крутых спусках
     move_y=0
     if(height < screen_height): move_y=(screen_height - height)/2
 
-    #calculate the initial coordinates of a rectangle
+    #вычисляем стартовую позицию блока
     coords = coordinates(angle,move_x,move_y)
 
-    #opening a window
+    #открываем окно
     pygame.init()
-    print("opening window ",screen_width," x ",screen_height)
+    print("открываем окно ",screen_width," x ",screen_height)
     global screen
     screen = pygame.display.set_mode((screen_width,screen_height))
-    pygame.display.set_caption("friction force")
-    font = pygame.font.Font('Ubuntu.ttf', 30)
+    pygame.display.set_caption("сила трения")
+    font = pygame.font.Font('Ubuntu.ttf', 30)#для вывода текста в окне
 
-    # Define some colors
+    #задаем необходимые цвета
     global BLACK,WHITE,GREEN,RED,BLUE
     BLACK = ( 0, 0, 0)
     WHITE = ( 255, 255, 255)
@@ -164,53 +166,55 @@ def main():
     RED = ( 255, 0, 0)
     BLUE = (0, 0, 255)
 
-    #starting the main render loop
+    #запускаем главный цикл отрисовки
     window_open = True
     render = True
-    im_centered = False #has the block reached the center of the screen
+    im_centered = False #доехал ли блок до центра окна
     clock = pygame.time.Clock()
-    distance = 0 #travel
+    distance = 0 #пройденный путь
     time = 0 
     while window_open:
         for event in pygame.event.get():
-            #close button closes the window
+            #крестик закрывает окно
             if event.type == pygame.QUIT:
                 window_open = False
         
         while render:
-            #duplicate the same loop here or  else the close button breaks
+            #дубликат того же цикла, чтоб крестик закрывал окно
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     window_open = False
                     render = False
-            time+=1/60
+            time+=1/60 #60 кардров в секунду
             speed=speed0 + acceleration*time
             if (speed<0): speed=0
 
             distance = speed0*time + (acceleration*math.pow(time,2))/2
+            #оптическая иллюзия что штрихи движутся бесконечно влево хотя они время от времени возвращаются на исходную позицию
             track_distance = -1 * distance % 20
 
             screen.fill(WHITE)
             pygame.draw.line(screen, GREEN, [0+move_x, 0+move_y], [width+move_x, height+move_y], 5)
             
+            #если блок не в центре, то...
             if (im_centered==False):
-                #we move the block
+                #...двигаем блок
                 draw_track(angle,0,move_x,move_y)
                 d_x,d_y=draw_rect(angle,coords,distance)
                 if (d_x>0.45*screen_width) or (d_y>0.45*screen_height):
                     im_centered=True
-                    frozen_distance = distance # freeze the position of the block
+                    frozen_distance = distance #заморозка позиции блока на экране
             else:
-                #we move the lines
+                #иначе двигаем штрихи
                 draw_track(angle,track_distance,move_x,move_y)
                 d_x,d_y=draw_rect(angle,coords,frozen_distance)
 
 
-            string_time="time:"+str(round(time,6))
-            string_speed="speed:"+str(round(speed,6))
-            string_distance="distance:"+str(round(distance,6))
-            string_force="force:"+str(round(force,6))
-            string_acceleration="acceleration:"+str(round(acceleration,6))
+            string_time="время:"+str(round(time,6))
+            string_speed="скорость:"+str(round(speed,6))
+            string_distance="расстояние:"+str(round(distance,6))
+            string_force="сита трения:"+str(round(force,6))
+            string_acceleration="ускорение:"+str(round(acceleration,6))
 
             screen.blit(font.render(string_time, True, BLUE),(screen_width-300,0))
             screen.blit(font.render(string_speed, True, BLUE),(screen_width-300,25))
@@ -219,12 +223,12 @@ def main():
             screen.blit(font.render(string_acceleration, True, RED),(10,screen_height-40))
 
             if (speed<=0):
-                screen.blit(font.render("FINISHED",True,BLACK),(10,screen_height-90))
+                screen.blit(font.render("ФИНИШ",True,BLACK),(10,screen_height-90))
                 render = False
 
-            pygame.display.flip()
+            pygame.display.flip()#отрсовка всего что сверху
 
-        clock.tick(60)
+        clock.tick(60)#60 кадров/сек
          
     pygame.quit()
 
